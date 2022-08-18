5fa1304a20321cabbe34f4244e8757613dc070ae
yv989c <yv989c@gmail.com>
2022-08-18 00:52:16 -0400

    chore: imported source files

---
+++ src/QueryableValues.SqlServer.EF6/QueryableValuesCommandInterceptor.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Data.Common;
+using System.Data.Entity.Infrastructure.Interception;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.Caching;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace BlazarTech.QueryableValues
+{
+    sealed class QueryableValuesCommandInterceptor : IDbCommandInterceptor
+    {
+        private static readonly MemoryCache Cache = new MemoryCache(
+            "BlazarTech.QueryableValues.EF6",
+            new NameValueCollection
+            {
+                { "CacheMemoryLimitMegabytes", "10" }
+            });
+
+        private static readonly Regex Regex1 = new Regex(@"'" + QueryableValuesDbContextExtensions.InternalId + @"(?<DT>[a-z]{3,})'\s*=\s*@(?<V>.+?)\s*(?=\))", RegexOptions.CultureInvariant | RegexOptions.Compiled);
+        private static readonly Regex Regex2 = new Regex(@"SELECT\s+TOP\s+\(\s*@(?<T>.+?)\s*\)", RegexOptions.CultureInvariant | RegexOptions.RightToLeft | RegexOptions.Compiled);
+
+        private static void TransformCommand(DbCommand command)
+        {
+            var originalCommandText = command.CommandText;
+
+            if (originalCommandText is null || !originalCommandText.Contains(QueryableValuesDbContextExtensions.InternalId))
+            {
+                return;
+            }
+
+            if (!(command is SqlCommand sqlCommand))
+            {
+                throw new InvalidOperationException("QueryableValues only works with a SQL Server provider.");
+            }
+
+            var entry = (InterceptedCommandData)Cache.Get(originalCommandText);
+
+            if (entry is null)
+            {
+                var sb = new StringBuilder();
+                var xmlParameterNames = new HashSet<string>();
+                var matches = Regex1.Matches(originalCommandText);
+                var lastStartIndex = 0;
+
+                foreach (Match match in matches)
+                {
+                    var match2 = Regex2.Match(originalCommandText, match.Index);
+                    if (match2.Success)
+                    {
+                        var valueParameterName = match.Groups["V"].Value;
+
+                        xmlParameterNames.Add(valueParameterName);
+
+                        sb.Append(originalCommandText.Substring(lastStartIndex, match2.Index - lastStartIndex));
+                        sb.Append("SELECT TOP (@");
+                        sb.Append(match2.Groups["T"].Value);
+                        sb.Append(") I.value(");
+
+                        var dataType = match.Groups["DT"].Value;
+
+                        switch (dataType)
+                        {
+                            case "short":
+                                sb.Append("'. cast as xs:short?', 'smallint'");
+                                break;
+                            case "int":
+                                sb.Append("'. cast as xs:integer?', 'int'");
+                                break;
+                            case "long":
+                                sb.Append("'. cast as xs:integer?', 'bigint'");
+                                break;
+                            default:
+                                throw new NotImplementedException(dataType);
+                        }
+
+                        sb.Append(") AS [C1] FROM @").Append(valueParameterName).Append(".nodes('/R/V') N(I)");
+
+                        lastStartIndex = match.Index + match.Length;
+                    }
+                }
+
+                sb.Append(originalCommandText.Substring(lastStartIndex));
+
+                entry = new InterceptedCommandData
+                {
+                    XmlParameterNames = xmlParameterNames,
+                    CommandText = sb.ToString()
+                };
+
+                var cachePolicy = new CacheItemPolicy
+                {
+                    SlidingExpiration = TimeSpan.FromHours(12)
+                };
+
+                Cache.Add(originalCommandText, entry, cachePolicy);
+            }
+
+            foreach (SqlParameter parameter in sqlCommand.Parameters)
+            {
+                if (entry.XmlParameterNames.Contains(parameter.ParameterName))
+                {
+                    parameter.SqlDbType = System.Data.SqlDbType.Xml;
+                    parameter.Size = -1;
+                }
+            }
+
+            sqlCommand.CommandText = entry.CommandText;
+        }
+
+        public void NonQueryExecuted(DbCommand command, DbCommandInterceptionContext<int> interceptionContext)
+        {
+        }
+
+        public void NonQueryExecuting(DbCommand command, DbCommandInterceptionContext<int> interceptionContext)
+        {
+            TransformCommand(command);
+        }
+
+        public void ReaderExecuted(DbCommand command, DbCommandInterceptionContext<DbDataReader> interceptionContext)
+        {
+        }
+
+        public void ReaderExecuting(DbCommand command, DbCommandInterceptionContext<DbDataReader> interceptionContext)
+        {
+            TransformCommand(command);
+        }
+
+        public void ScalarExecuted(DbCommand command, DbCommandInterceptionContext<object> interceptionContext)
+        {
+        }
+
+        public void ScalarExecuting(DbCommand command, DbCommandInterceptionContext<object> interceptionContext)
+        {
+            TransformCommand(command);
+        }
+
+        private class InterceptedCommandData
+        {
+            public string CommandText { get; set; }
+            public HashSet<string> XmlParameterNames { get; set; }
+        }
+    }
+}
--- src/QueryableValues.SqlServer.EF6/QueryableValuesDbContextExtensions.cs
+++ src/QueryableValues.SqlServer.EF6/QueryableValuesDbContextExtensions.cs
@@ -1,12 +1,71 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure.Interception;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BlazarTech.QueryableValues
 {
     public static class QueryableValuesDbContextExtensions
     {
+        internal const string InternalId = "qv-jDDd5B3uLYjJD9OnH1iEKtiHcaIcgo8VxoMN4vri0Rk-";
+
+        private static readonly ConcurrentDictionary<Type, Type> DbSetByDbContext = new ConcurrentDictionary<Type, Type>();
+        private static readonly Func<Type, Type> DbSetByDbContextFactory = dbContextType =>
+            dbContextType
+                .GetProperties()
+                .Select(i => i.PropertyType)
+                .Where(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(DbSet<>))
+                .Select(i => i.GenericTypeArguments[0])
+                .FirstOrDefault();
+
+        private static readonly Func<IQueryable<object>, string, IQueryable<short>> InnerQueryShort = (dbSet, valuesXml) =>
+            from i in dbSet
+            where InternalId + "short" == valuesXml
+            select (short)1;
+
+        private static readonly Func<IQueryable<object>, string, IQueryable<int>> InnerQueryInt = (dbSet, valuesXml) =>
+            from i in dbSet
+            where InternalId + "int" == valuesXml
+            select 1;
+
+        private static readonly Func<IQueryable<object>, string, IQueryable<long>> InnerQueryLong = (dbSet, valuesXml) =>
+            from i in dbSet
+            where InternalId + "long" == valuesXml
+            select 1L;
+
+        static QueryableValuesDbContextExtensions()
+        {
+            DbInterception.Add(new QueryableValuesCommandInterceptor());
+        }
+
+        private static IQueryable<T> AsQueryableValues<T>(
+            DbContext dbContext,
+            string valuesXml,
+            int valuesCount,
+            Func<IQueryable<object>, string, IQueryable<T>> getInnerQuery)
+        {
+            var entityType = DbSetByDbContext.GetOrAdd(dbContext.GetType(), DbSetByDbContextFactory);
+            var dbSet = (IQueryable<object>)dbContext.Set(entityType);
+            var innerQuery = getInnerQuery(dbSet, valuesXml);
+            var query = innerQuery.Take(() => valuesCount);
+            return query;
+        }
+
+        public static IQueryable<short> AsQueryableValues(this DbContext dbContext, ICollection<short> values)
+        {
+            return AsQueryableValues(dbContext, "<R><V>1</V><V>3</V></R>", 2, InnerQueryShort);
+        }
+
+        public static IQueryable<int> AsQueryableValues(this DbContext dbContext, ICollection<int> values)
+        {
+            return AsQueryableValues(dbContext, "<R><V>1</V><V>3</V></R>", 2, InnerQueryInt);
+        }
+
+        public static IQueryable<long> AsQueryableValues(this DbContext dbContext, ICollection<long> values)
+        {
+            return AsQueryableValues(dbContext, "<R><V>1</V><V>3</V></R>", 2, InnerQueryLong);
+        }
     }
 }
