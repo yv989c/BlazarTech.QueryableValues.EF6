0c55cc05b20aae5cb24564d32a0383a1b25622b3
yv989c <yv989c@gmail.com>
2022-08-21 13:59:57 -0400

    feat: wip

---
+++ tests/QueryableValues.EF6.SqlServer.Tests.DotNetFramework/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace QueryableValues.EF6.SqlServer.Tests.DotNetFramework
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ tests/QueryableValues.EF6.SqlServer.Tests/Integration/SelectTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using BlazarTech.QueryableValues.DbContext.DatabaseFirst;
+
+namespace BlazarTech.QueryableValues.Integration
+{
+    public class SelectTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            //var db = new TestDbContext("metadata=res://*/Model1.csdl|res://*/Model1.ssdl|res://*/Model1.msl;provider=System.Data.SqlClient;provider connection string=\"data source=.\\SQLEXPRESS;initial catalog=Test;integrated security=True;MultipleActiveResultSets=True;App=EntityFramework\"");
+            //var result = db.MyEntity.ToList();
+        }
+    }
+}
